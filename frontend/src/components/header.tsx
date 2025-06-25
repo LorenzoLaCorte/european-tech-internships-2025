839afc5b3d6a727ae369f4af9f12d5a6f5633706
LLaCorte <lorenzolacorte99@gmail.com>
2025-06-25 11:21:59 +0200

    Fix logo bug

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,7 +1,9 @@
+import logo from '@/static/logo.png';
+
 export function Header() {
   return (
     <header className="w-full flex justify-center items-center py-4 border-b bg-white dark:bg-slate-950">
-      <img src="/src/static/logo.png" alt="Logo" className="h-10" />
+      <img src={logo} alt="Logo" className="h-10" />
     </header>
   );
 }
